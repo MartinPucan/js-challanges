811ac72c757c56ed0a65cbae411d9022ae5d2c62
MartinPucan <pucan.m@seznam.cz>
2021-03-17 00:19:22 +0100

    created basic webpack setup

---
+++ src/js/1_sum.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function sum(...args) {
+    let total;
+    total = args.reduce((acc, elem) => acc + elem);
+    console.log(total);
+}
+
+sum(1,3);
+sum(10, 20, 5);
+sum(2, 5, 80, 1, 10, 12);
