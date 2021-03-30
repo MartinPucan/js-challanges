7e6b9302f57d8ccd1531b2784b5e9e7f6826102a
MartinPucan <pucan.m@seznam.cz>
2021-03-30 23:58:24 +0200

    added factorial && removed unique elems

---
+++ src/js/bonus/factorial.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function factorial(number) {
+    console.log(number);
+
+    if (number === 0) {
+        return 1;
+    } else {
+        return factorial(number - 1) * number;
+
+    }
+}
+
+console.log(factorial(5));
--- src/js/bonus/uniqueElem.js
+++ src/js/bonus/uniqueElem.js
@@ -1,12 +1,15 @@
 "use strict";
 
-function getUniqueElement(array) {
-    return [...new Set(array)];
+const getUniqueElement = array => {
+    const uniqueElem = [...new Set(array)];
+
+    return Array.from(uniqueElem);
 }
 
 const results = [
     "2:1", "2:1", "1:1", "0:1", "7:1"
 ];
 
-console.log(results); // array with all items
-console.log(getUniqueElement(results)); // array with unique items
+console.log(typeof results); // array with all items
+console.log(typeof getUniqueElement(results)); // array with unique items
+console.log(Array.isArray(getUniqueElement(results))); // array with unique items
