c896b774c0a8e0882b3277242152023e0a8c811a
MartinPucan <pucan.m@seznam.cz>
2021-04-05 14:12:10 +0200

    another solution for merge arrays

--- src/js/11_spreadOperator.js
+++ src/js/11_spreadOperator.js
@@ -8,7 +8,8 @@ b = [4, 5];
 c = [8, 9, 10];
 d = 11;
 
-
 let array = [0, ...a, 3, ...b, 6, 7, ...c, d];
-
 console.log(array)
+
+let newArray = a.concat([0], [3], b, [6,7], c, [d]);
+console.log(newArray)
