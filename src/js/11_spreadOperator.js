7f26f49f3faead8dc32ef65ee471c2ce78288a75
MartinPucan <pucan.m@seznam.cz>
2021-04-05 21:48:20 +0200

    removed declaring variables:

--- src/js/11_spreadOperator.js
+++ src/js/11_spreadOperator.js
@@ -1,12 +1,9 @@
 "use strict";
 
-var a, b, c, d;
-
-a = [1, 2];
-
-b = [4, 5];
-c = [8, 9, 10];
-d = 11;
+let a = [1, 2];
+let b = [4, 5];
+let c = [8, 9, 10];
+let d = 11;
 
 let array = [0, ...a, 3, ...b, 6, 7, ...c, d];
 console.log(array)
