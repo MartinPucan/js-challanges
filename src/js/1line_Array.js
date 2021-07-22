70e7d9a3367a29fdabb61f191db13fdc4bd3743d
MartinPucan <pucan.m@seznam.cz>
2021-07-22 14:23:30 +0200

    refactoring

--- src/js/1line_Array.js
+++ src/js/1line_Array.js
@@ -2,8 +2,16 @@
 
 const arr1 = [];
 const arr2 = [1, 2];
+const arr3 = [1, 2];
 
-const isEmpty = array => { Array.isArray(array) && array.length === 0 ? console.log(`Array is empty`) : console.log(`Array is not Empty`) }
+const isEmpty = array => Array.isArray(array) && array.length === 0 ? console.log(`Array is empty`) : console.log(`Array is not Empty`)
 
 isEmpty(arr1)
 isEmpty(arr2)
+
+// Compare two arrays
+
+const compareArray = (array1, array2) => JSON.stringify(array1) === JSON.stringify(array2) ? console.log(`Arrays are equal`) : console.log(`Arrays are not equal`)
+
+compareArray(arr1, arr2)
+compareArray(arr3, arr2)
